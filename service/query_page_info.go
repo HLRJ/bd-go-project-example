df3f83e33db6fe53e954607925c1f444092238ab
HLRJ <withinwzx@gmail.com>
2022-05-12 11:29:25 +0800

    阅读了service下面的代码query_page_info  增加了注释

--- service/query_page_info.go
+++ service/query_page_info.go
@@ -6,12 +6,12 @@ import (
 	"sync"
 )
 
-type PageInfo struct {
-	Topic    *repository.Topic
+type PageInfo struct { //对外提供服务
+	Topic    *repository.Topic //进一步封装数据
 	PostList []*repository.Post
 }
 
-func QueryPageInfo(topicId int64) (*PageInfo, error) {
+func QueryPageInfo(topicId int64) (*PageInfo, error) { //PageInfo
 	return NewQueryPageInfoFlow(topicId).Do()
 }
 
@@ -21,35 +21,35 @@ func NewQueryPageInfoFlow(topId int64) *QueryPageInfoFlow {
 	}
 }
 
-type QueryPageInfoFlow struct {
+type QueryPageInfoFlow struct { //包内使用
 	topicId  int64
-	pageInfo *PageInfo
+	pageInfo *PageInfo //方便给对外服务的结构体赋值
 
-	topic   *repository.Topic
-	posts   []*repository.Post
+	topic *repository.Topic
+	posts []*repository.Post
 }
 
-func (f *QueryPageInfoFlow) Do() (*PageInfo, error) {
+func (f *QueryPageInfoFlow) Do() (*PageInfo, error) { //方法 返回一个PageInfo指针   ,作用是判断和验证信息
 	if err := f.checkParam(); err != nil {
 		return nil, err
 	}
-	if err := f.prepareInfo(); err != nil {
+	if err := f.prepareInfo(); err != nil { //封装到小写
 		return nil, err
 	}
-	if err := f.packPageInfo(); err != nil {
+	if err := f.packPageInfo(); err != nil { //封装到大写
 		return nil, err
 	}
 	return f.pageInfo, nil
 }
 
-func (f *QueryPageInfoFlow) checkParam() error {
+func (f *QueryPageInfoFlow) checkParam() error { //检查参数
 	if f.topicId <= 0 {
 		return errors.New("topic id must be larger than 0")
 	}
 	return nil
 }
 
-func (f *QueryPageInfoFlow) prepareInfo() error {
+func (f *QueryPageInfoFlow) prepareInfo() error { //将信息封装到QueryPageInfoFlow结构体
 	//获取topic信息
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -68,7 +68,7 @@ func (f *QueryPageInfoFlow) prepareInfo() error {
 	return nil
 }
 
-func (f *QueryPageInfoFlow) packPageInfo() error {
+func (f *QueryPageInfoFlow) packPageInfo() error { //将小写的数据封装为大写的  对包外提供服务
 	f.pageInfo = &PageInfo{
 		Topic:    f.topic,
 		PostList: f.posts,
